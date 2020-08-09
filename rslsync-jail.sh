27db76bdf3e799ffe3120b23adf5df9f308d3ed1
Basil Hendroff <63370329+basilhendroff@users.noreply.github.com>
2020-08-09 17:00:43 +0800

    Update rslsync-jail.sh

--- rslsync-jail.sh
+++ rslsync-jail.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Build an iocage jail under FreeNAS 11.3 using the current release of Resilio Sync
-# https://github.com/basilhendroff/freenas-iocage-rslsync
+# Build an iocage jail under FreeNAS 11.3-12.0 using the current release of Resilio Sync
+# git clone https://github.com/basilhendroff/freenas-iocage-rslsync
 
 # Check for root privileges
 if ! [ $(id -u) = 0 ]; then
@@ -15,29 +15,25 @@ fi
 #####
 
 # Initialize defaults
-JAIL_NAME="rslsync"
 JAIL_IP=""
+JAIL_INTERFACES=""
 DEFAULT_GW_IP=""
 INTERFACE="vnet0"
 VNET="on"
-RELEASE="11.3-RELEASE"
-JAILS_MOUNT=$(zfs get -H -o value mountpoint $(iocage get -p)/iocage)
 POOL_PATH=""
+JAIL_NAME="rslsync"
+CONFIG_NAME="rslsync-config"
 CONFIG_PATH=""
 DATA_PATH=""
 
-TIME_ZONE=""
-HOST_NAME=""
-DATABASE="mariadb"
-DB_PATH=""
-PORTS_PATH=""
-DL_FLAGS=""
-DNS_SETTING=""
-
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "${SCRIPT}")
-. "${SCRIPTPATH}"/rslsync-config
+if ! [ -e "${SCRIPTPATH}"/"${CONFIG_NAME}" ]; then
+  echo "${SCRIPTPATH}/${CONFIG_NAME} must exist."
+  exit 1
+fi
+. "${SCRIPTPATH}"/"${CONFIG_NAME}"
 INCLUDES_PATH="${SCRIPTPATH}"/includes
+
+JAILS_MOUNT=$(zfs get -H -o value mountpoint $(iocage get -p)/iocage)
 
 # Check for rslsync-config and set configuration
 if ! [ -e "${SCRIPTPATH}"/rslsync-config ]; then
@@ -55,6 +51,10 @@ fi
 if [ -z "${JAIL_IP}" ]; then
   echo 'Configuration error: JAIL_IP must be set'
   exit 1
+fi
+if [ -z "${JAIL_INTERFACES}" ]; then
+  echo 'JAIL_INTERFACES not set, defaulting to: vnet0:bridge0'
+  JAIL_INTERFACES="vnet0:bridge0"
 fi
 if [ -z "${DEFAULT_GW_IP}" ]; then
   echo 'Configuration error: DEFAULT_GW_IP must be set'
@@ -79,11 +79,31 @@ then
   echo "CONFIG_PATH and DATA_PATH must be different!"
   exit 1
 fi
-
 if [ "${DATA_PATH}" = "${POOL_PATH}" ] || [ "${CONFIG_PATH}" = "${POOL_PATH}" ]
 then
   echo "FILES_PATH and CONFIG_PATH must all be different from POOL_PATH!"
   exit 1
+fi
+
+# If CONFIG_PATH wasn't set in nextcloud-config, set it
+if [ -z "${CONFIG_PATH}" ]; then
+  CONFIG_PATH="${POOL_PATH}"/apps/rslsync/config
+fi
+# If DATA_PATH wasn't set in nextcloud-config, set it
+if [ -z "${DATA_PATH}" ]; then
+  DATA_PATH="${POOL_PATH}"/apps/rslsync/data
+fi
+
+# Extract IP and netmask, sanity check netmask
+IP=$(echo ${JAIL_IP} | cut -f1 -d/)
+NETMASK=$(echo ${JAIL_IP} | cut -f2 -d/)
+if [ "${NETMASK}" = "${IP}" ]
+then
+  NETMASK="24"
+fi
+if [ "${NETMASK}" -lt 8 ] || [ "${NETMASK}" -gt 30 ]
+then
+  NETMASK="24"
 fi
 
 #####
@@ -102,7 +122,7 @@ cat <<__EOF__ >/tmp/pkg.json
 __EOF__
 
 # Create the jail and install previously listed packages
-if ! iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r "${RELEASE}" ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}"
+if ! iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r "${RELEASE}" interfaces="${JAIL_INTERFACES}" ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}"
 then
 	echo "Failed to create jail"
 	exit 1
